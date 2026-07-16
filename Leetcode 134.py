af68655ace969d298136a3215f890b7c7ca86a6b
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-16 09:18:42 +0530

    Update Leetcode 134.py

--- Leetcode 134.py
+++ Leetcode 134.py
@@ -1,4 +1,10 @@
 # Leetcode Question 134 (Gas Station)
 
 class Solution:
-  def gas
+  def cancompletecircuit(self, gas: List[int]):
+    if sum(gas)< sum(cost):
+      return -1
+
+
+    for i in range(len(gas)):
+      sumgas += gas[i]
