b1b9824dfa9003742b5dd021e16a18954cdc7eac
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-16 09:46:04 +0530

    Update Leetcode 55.py

--- Leetcode 55.py
+++ Leetcode 55.py
@@ -1,1 +1,11 @@
 #Leetcode Question 55 (Jump Game)
+
+class Solution:
+  def canjump():
+    target = len(nums)-1
+
+
+    for i in range(len(nums)-2,-1,-1):
+      if i+nums[i]>=goal:
+        goal=i
+    return goal == 0
