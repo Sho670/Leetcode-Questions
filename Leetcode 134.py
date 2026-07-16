62247779c2466e5d6040b6c6bc3f3d3aac2ebca3
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-16 09:20:55 +0530

    Update Leetcode 134.py

--- Leetcode 134.py
+++ Leetcode 134.py
@@ -2,9 +2,22 @@
 
 class Solution:
   def cancompletecircuit(self, gas: List[int]):
+    
     if sum(gas)< sum(cost):
       return -1
 
 
+    sumgas = 0
+    sumcost = 0
+
     for i in range(len(gas)):
       sumgas += gas[i]
+      sumcost += cost[i]
+
+      if sumgas< sumcost:
+
+        index = i+1
+        sumgas=0
+        sumcost=0
+
+    return index
