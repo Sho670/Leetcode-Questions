e39161971a006f0c9bd15c94d5c89ac2a84408b4
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-17 11:36:33 +0530

    Update Leetcode 735.py

--- Leetcode 735.py
+++ Leetcode 735.py
@@ -5,4 +5,15 @@ def asteriodcollision(self, a:list[int]):
   for i in a:
     if i>0:
       st.append(i)
+    else:
+      while st and st[-1]>0 and abs(i)>st[-1]:
+        st.pop()
+      if st and abs(i)==st[-1]:
+        st.pop()
+        continue
+      elif st and abs(i)<st[-1]:
+        continue
+      else:
+        st.append(i)
+    return st
       
