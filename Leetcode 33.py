7d844e7ef180f6391a0c18f19ec8404cfce6628d
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-17 12:08:58 +0530

    Update Leetcode 33.py

--- Leetcode 33.py
+++ Leetcode 33.py
@@ -1,1 +1,20 @@
 #Leetcode Question 33 (Search in Rotated Sorted Array)
+
+class Solution:
+  def search():
+    i=0
+    j=len(nums)-1
+    while i<=j:
+      mid = (i+j) // 2
+      if num [mid]==target:
+        return mid
+      if nums[i]<nums[mid]:
+        if nums[i]<=target<nums[mid]:
+          j=mid
+        else:
+          i=mid+1
+      else:
+        if nums[mid]<target<=nums[j]:
+          i=mid+1
+        else:
+          
