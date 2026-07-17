4e052d5459f38e29f9c0d2802b95c0569992babd
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-17 11:42:38 +0530

    Update Leetcode 239.py

--- Leetcode 239.py
+++ Leetcode 239.py
@@ -4,4 +4,11 @@
 
 class Solution:
   def maxslidingwindow():
+    q=[]
+    answer = []
+    for i in range(len(nums)):
+      if q and q[0]< i-k+1:
+        q.pop()
+      while q and nums[q[-1]<nums[i]]:
+        q.pop()
     
