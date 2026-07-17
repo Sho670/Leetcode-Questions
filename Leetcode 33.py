85c66d27cec73247b82d88fd57f9bf9c56e93ed3
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-17 12:29:00 +0530

    Update Leetcode 33.py

--- Leetcode 33.py
+++ Leetcode 33.py
@@ -1,13 +1,20 @@
 #Leetcode Question 33 (Search in Rotated Sorted Array)
 
+# Approach: Binary Search Tree
+
 class Solution:
+  
   def search():
+    
     i=0
     j=len(nums)-1
+    
     while i<=j:
       mid = (i+j) // 2
+      
       if num [mid]==target:
         return mid
+        
       if nums[i]<nums[mid]:
         if nums[i]<=target<nums[mid]:
           j=mid
