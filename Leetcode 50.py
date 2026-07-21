1f7a5a3b6500f7138573d157737dfaed6d7b6f2c
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-21 09:21:18 +0530

    Update Leetcode 50.py

--- Leetcode 50.py
+++ Leetcode 50.py
@@ -1,10 +1,14 @@
 # Leetcode Question 50 (Pow(x,n))
 
 class Solution(object):
+  
   def myPow(self, x, n):
+    
     if (n<0):
       x=1/x
       n=-n
+
+    
     if (n==0):
       return 1
 
@@ -12,4 +16,6 @@ class Solution(object):
 
     if (n%2==0):
       return dp*dp
+
+    
     return dp*dp*x
