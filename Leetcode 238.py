2183c935e2eb33e0af107d4c9962c2d54c419ff5
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-21 09:25:52 +0530

    Update Leetcode 238.py

--- Leetcode 238.py
+++ Leetcode 238.py
@@ -1,4 +1,14 @@
 # Leetcode Question 238 (Product of array except self)
 
 def productexceptself(self,nums: List[int]):
+  d=1
+  result=[]
+
+for i in range(len(nums)):
+  result.append(d)
+
+  d=d 8 numa[i]
+
+for i in range(len(nums)-2,-1,-1):
+  result
   
