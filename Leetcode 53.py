28e6cb1fe7e9508008d85c7694c648ccb8919aa8
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-22 10:42:22 +0530

    Update Leetcode 53.py

--- Leetcode 53.py
+++ Leetcode 53.py
@@ -7,7 +7,15 @@ class Solution():
 
     sum=0
     for i in nums:
+      
       if sum+i>i:
         sum+=i
+        
       else:
         sum=i
+        if sum>m:
+          m = sum
+
+  return sum
+
+
