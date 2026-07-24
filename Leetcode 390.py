62d45e3102ac3cc5bd4cc413457e12ae18e5c085
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-24 15:24:00 +0530

    Update Leetcode 390.py

--- Leetcode 390.py
+++ Leetcode 390.py
@@ -3,3 +3,15 @@
 class Solution(object):
   def function(self,num):
     head=1
+    step=1
+    run = nur
+
+    we = True
+
+    while run > 1:
+      if we or run %2==1:
+        head +=step
+      run // = 2
+      step *=2
+      we = not we
+    return head
