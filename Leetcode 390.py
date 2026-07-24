4a2af1512e0a0cb4c64edf1aad1af053778e24d4
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-24 15:24:44 +0530

    Update Leetcode 390.py

--- Leetcode 390.py
+++ Leetcode 390.py
@@ -13,9 +13,12 @@ class Solution(object):
     we = True
 
     while run > 1:
+      
       if we or run %2==1:
+        
         head +=step
       run // = 2
       step *=2
       we = not we
+      
     return head
