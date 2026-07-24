2e2aab2395aced17c3ae394b8e264214e8fd6aca
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-24 15:17:19 +0530

    Update Leetcode 04.py

--- Leetcode 04.py
+++ Leetcode 04.py
@@ -9,5 +9,21 @@ def foursum(self, nums: list-> list[int]):
       if i>0 and nums[i] == nums[i-1]:
           continue
 
+      for l in range(i+1, len(nums)-2):
+          if l>i+1 and nums[l]==nums[l-1]:
+              continue
+
+      j = i+1
+      k = len(nums)-1
+
+      while j<k:
+        sum = nums[i]+nums[j]+nums[k]
+        if sum == target:
+          answer. append([nums[i],nums[j],nums[k]])
+          j+=1
+          k-=1
+          while j<k and nums[j]==nums[j-1]:
+            j+=1
+
       
   
