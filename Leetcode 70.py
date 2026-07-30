f70917d8dcad751831b3ac759e5fbed3aacc7839
Shohom <181641785+Sho670@users.noreply.github.com>
2026-07-30 19:25:37 +0530

    Update Leetcode 70.py

--- Leetcode 70.py
+++ Leetcode 70.py
@@ -1,5 +1,24 @@
 # Leetcode 70 (Climbing Stairs)
 
+# Approach Used: Dynamic Programming..
+
 class Solution:
   def climbingstairs(self, nums-> int):
+
+    dp= [-1]*(n+1)
+
+    def f(n):
+      if (n==0):
+        return 1
+      if (n<0):
+        return 0
+      if (dp[n]!=-1):
+        return dp[n]
+
+    dp[n]=f(n-1)+f(n-2)
+
+    return dp[n]
+
+    return f(n)
+    
     
