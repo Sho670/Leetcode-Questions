93c195c79db5acbcd3d72a4cc32b4c536af75956
Shohom <181641785+Sho670@users.noreply.github.com>
2026-08-12 19:33:07 +0530

    Update Leetcode 344.py

--- Leetcode 344.py
+++ Leetcode 344.py
@@ -1,2 +1,9 @@
 # Leetcode Question 344 (Reversing a String)
 
+def reversestring(self, nums):
+  def f(left, right):
+    if (left >= right):
+      return s[left],s[right] = s[right], s[left]
+      f(left+1, right-1)
+
+      f(0,len(s)-1)
