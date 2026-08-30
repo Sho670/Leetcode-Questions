755b7cf31283c99eb46649ce2f4108ac2e76fcc7
Shohom <181641785+Sho670@users.noreply.github.com>
2026-08-30 19:44:27 +0530

    Update Leetcode 18.py

--- Leetcode 18.py
+++ Leetcode 18.py
@@ -1,2 +1,16 @@
 # Leetcode Question 18 (Four Sum Problem)
 
+def foursum(self, list-> list[int])
+
+nums.sort()
+
+answer = []
+for i in range(0,len(nums)-3):
+  if i>0 and nums[i]==nums[i-1]:
+    continue
+
+
+  for l in range(i+1, len(nums)-2):
+    if l>i+1 and nums[l]==nums[l-1]:
+      continue
+
