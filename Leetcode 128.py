82507653bcfafe5f497acdff9f012e0a9fd8a481
Shohom <181641785+Sho670@users.noreply.github.com>
2026-09-10 22:45:59 +0530

    Update Leetcode 128.py

--- Leetcode 128.py
+++ Leetcode 128.py
@@ -3,3 +3,14 @@
 def longestconsecutive(self, List->[nums][int][int]):
     if len(nums)==0:
       return 0
+    s=set(nums)
+    m=1
+    for i in s:
+        if i-1 not in s:
+            c=1
+            while i+1 in s:
+                c+=1
+                i+=1
+            if m<c:
+                m=c
+    return m
